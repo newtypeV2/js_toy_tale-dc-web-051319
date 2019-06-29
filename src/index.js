418e4ef3fdc7d2a6671d8109430c12b8f8792309
newtypeV2 <gianlibunao@gmail.com>
2019-06-29 13:51:29 -0400

    Experimented with Datasets.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-const toyURL = "http://localhost:3000/toys";
-let testArray = [];
+const toyURL = "http://localhost:3000/toys/";
 
 
 class Toy{
@@ -13,26 +12,23 @@ class Toy{
   render(containerElement){
     const card = document.createElement("div");
     card.id = `Toy-${this.id}`;
-    card.dataset.toyID= this.id;
+    card.dataset.toyid= this.id;
+    card.dataset.toylikes = this.likes;
     card.classList.add("card");
-    // card.innerHTML=
-    // `<h2>${this.name}</h2>
-    // <img src=${this.image} />
-    // <p>${this.likes} Likes </p>`
-    const toyHeader = document.createElement("h2");
-    toyHeader.innerText = this.name;
-    const toyImage = document.createElement("img");
-    toyImage.src = this.image;
-    const toyLikes = document.createElement("p");
-    toyLikes.innerText = `${this.likes} Likes`;
-    const likeButton = document.createElement("button");
-    likeButton.innerText="Like <3";
-    likeButton.addEventListener("click",likeHandler);
-    card.appendChild(toyHeader);
-    card.appendChild(toyImage);
-    card.appendChild(toyLikes);
-    card.appendChild(likeButton);
+    card.innerHTML=
+    `<h2>${this.name}</h2>
+    <img src=${this.image} class="toy-avatar"/>
+    <p>${this.likes} Likes </p>
+    <button class="like-btn">Like <3</button>`
     containerElement.append(card);
+    const likeButton = card.querySelector("button.like-btn")
+    likeButton.addEventListener("click",likeHandler);
+  }
+
+  updateDOM(){
+    document.querySelector(`[data-toyid='${this.id}']`).dataset.toylikes = this.likes;
+    document.querySelector(`[data-toyid='${this.id}']`).querySelector("p").innerText = `${this.likes} Likes`;
+
   }
 }
 
@@ -40,12 +36,22 @@ function objectCreator(toy){
   
   let toyObj = new Toy (toy.id,toy.name,toy.image,toy.likes);
   toyObj.render(getToyContainer());
-  debugger
   
 }
 
-function likeHandler(){
-  console.log(`Card # ${e.currentTarget.parentElement.id} was liked`)
+function likeHandler(e)
+  e.currentTarget.parentElement.dataset
+  let likes = Number(e.currentTarget.parentElement.dataset.toylikes) + 1
+  fetch(toyURL+e.currentTarget.parentElement.dataset.toyid.toString(),{
+    method: 'PATCH',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({likes}),
+  })
+  .then(res => res.json())
+  .then(toy => {
+    let refToy = new Toy(toy.id,toy.name,toy.image,toy.likes);
+    refToy.updateDOM();
+  })
 }
 
 function init(){
