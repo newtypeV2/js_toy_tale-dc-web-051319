241fbfef59390ac82581b4fe0d438be1bf3fc40c
newtypeV2 <gianlibunao@gmail.com>
2019-06-29 12:23:30 -0400

    Refactoring using classes.

--- src/index.js
+++ src/index.js
@@ -1,105 +1,58 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
-const indexURL = "http://localhost:3000/toys";
+const toyURL = "http://localhost:3000/toys";
+let testArray = [];
 
 
-
-// YOUR CODE HERE
-
-
-
-addBtn.addEventListener('click', () => {
-  // hide & seek with the form
-  addToy = !addToy
-  if (addToy) {
-    toyForm.style.display = 'block'
-    // submit listener here
-  } else {
-    toyForm.style.display = 'none'
+class Toy{
+  constructor(id,name,image,likes){
+    this.id = id;
+    this.name = name;
+    this.image = image;
+    this.likes = likes;
   }
-})
 
-
-// OR HERE!
-
-function getJSON(url){
-  return fetch(url).then(response => response.json());
+  render(containerElement){
+    const card = document.createElement("div");
+    card.id = `Toy-${this.id}`;
+    card.dataset.toyID= this.id;
+    card.classList.add("card");
+    // card.innerHTML=
+    // `<h2>${this.name}</h2>
+    // <img src=${this.image} />
+    // <p>${this.likes} Likes </p>`
+    const toyHeader = document.createElement("h2");
+    toyHeader.innerText = this.name;
+    const toyImage = document.createElement("img");
+    toyImage.src = this.image;
+    const toyLikes = document.createElement("p");
+    toyLikes.innerText = `${this.likes} Likes`;
+    const likeButton = document.createElement("button");
+    likeButton.innerText="Like <3";
+    likeButton.addEventListener("click",likeHandler);
+    card.append(toyHeader,toyImage,toyLikes,likeButton);
+    containerElement.append(card);
+  }
 }
 
-function initIndexPage(){
-  getJSON(indexURL).then(toys => toys.forEach(renderInDOM));
+function objectCreator(toy){
+  
+  let toyObj = new Toy (toy.id,toy.name,toy.image,toy.likes);
+  toyObj.render(getToyContainer());
+  debugger
+  
 }
 
-
-function postData(url , data = {}) {
-  return fetch(url, {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-  })
-}
-
-
-function patchData(url , data = {}) {
-  return fetch(url, {
-      method: 'PATCH',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-  })
-}
-
-function likeHandler(e){
-  const newLike = Number(e.target.previousElementSibling.innerText)+1;
-  const editURL = "http://localhost:3000/toys/"+e.target.parentElement.id;
-  const editToy = { likes : newLike };
-  patchData(editURL,editToy);
-}
-
-function renderInDOM(toy){
-  const toyCollectionDiv = document.getElementById("toy-collection");
-  const toyCardDiv = document.createElement("div");
-  const toyName = document.createElement("h2");
-  const toyImg = document.createElement("img");
-  const toyP = document.createElement("p");
-  const toyLike = document.createElement("button");
-  toyCardDiv.classList.add("card");
-  toyImg.classList.add("toy-avatar");
-  toyLike.classList.add("like-btn");
-  toyCardDiv.id=toy.id;
-  toyName.innerText = toy.name;
-  toyImg.src = toy.image;
-  toyP.innerText = toy.likes;
-  toyLike.innerText="Like <3";
-  toyLike.addEventListener("click",likeHandler);
-  toyCardDiv.appendChild(toyName);
-  toyCardDiv.appendChild(toyImg);
-  toyCardDiv.appendChild(toyP);
-  toyCardDiv.appendChild(toyLike);
-  toyCollectionDiv.appendChild(toyCardDiv);
-}
-
-
-
-
-function toySubmitHandler(e){
-  const newToy = {name: e.target.name.value,
-                image: e.target.image.value,
-                likes: 0
-              };
-  postData(indexURL,newToy);
+function likeHandler(){
+  console.log(`Card # ${e.currentTarget.parentElement.id} was liked`)
 }
 
 function init(){
-  initIndexPage();
+  fetch(toyURL)
+  .then(res => res.json())
+  .then(toyArray => toyArray.forEach(objectCreator))
 }
 
-
+function getToyContainer(){
+  return document.getElementById("toy-collection");
+}
 
 document.addEventListener("DOMContentLoaded",init);
-toyForm.addEventListener("submit",toySubmitHandler);
-
